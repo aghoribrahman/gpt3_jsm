be387e7663cbc325130047d55b8565fa784e4c2e
Mohit Trivedi <78268728+aghoribrahman@users.noreply.github.com>
2023-07-26 12:28:30 +0530

    first

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './article.css'
+
+
+const Article = () => {
+  return(
+    <div>
+      <h1>GPT-3</h1>
+    </div>
+    )
+};
+
+export default Article
---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './blog.css'
+
+
+const Blog = () => {
+  return(
+    <div>
+      <h1>Blog</h1>
+    </div>
+    )
+};
+
+export default Blog
