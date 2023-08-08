0893514ad92d7515646d30f647a2e72497048492
aghoribrahman <mohit.4033@gmail.com>
2023-08-08 17:01:33 +0530

    final touch

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -4,8 +4,14 @@ import './cta.css'
 
 const Cta = () => {
   return(
-    <div>
-      <h1>Cta</h1>
+    <div className='gpt3__cta'>
+      <div className='gpt3__cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h2>Register today & start exploring the endless possiblities.</h2>
+      </div>
+      <div className='gpt3__cta-btn'>
+        <button type='button'>Get Started</button>
+      </div>
     </div>
     )
 };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,11 +9,12 @@ const Navbar = () => {
     return(
     <>
     <p><a href="#home">Home</a></p>
-    <p><a href="#2wgpt3">What is GPT</a></p>
+    <p><a href="#wgpt3">What is GPT</a></p>
     <p><a href="#possibility">Open AI</a></p>
     <p><a href="#features">Case Studies</a></p>
     <p><a href="#blog">Library</a></p>
-    </>)
+    </>
+    )
   }
   return(
     <div className='gpt3__navbar'>
